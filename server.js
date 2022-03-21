e21a5737c3051dc9b6e589313f9cf9895c509f9b
Dmitriyhk <dimassik50@gmail.com>
2022-03-21 17:39:16 +0300

    m

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.listen(port, () => console.log('work'))
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.json("build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
   });
