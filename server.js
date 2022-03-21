d831969cfc5044fcd940abd45c8a967adca65644
Dmitriyhk <dimassik50@gmail.com>
2022-03-21 16:43:43 +0300

    :(((

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require('path');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
@@ -15,7 +15,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
+app.listen(port, () => console.log('work'))
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './build', '/index.html'));
